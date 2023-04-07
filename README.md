2dde71158c6a67a6a0e8b5be637c1421212f9e6c
mhmtalignc <gncmhmtali@gmail.com>
2023-04-07 14:26:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<h3>Car App</h3>
+<p>This project is a front-end web design for a car dealership website. It has been created using HTML5, CSS3, and JavaScript.</p>
+
+![](Auto.gif)
---
+++ main.js
@@ -0,0 +1,9 @@
+
+function imgSlider(e){
+    document.querySelector('.mercedes').src = e;
+}
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+
+}
